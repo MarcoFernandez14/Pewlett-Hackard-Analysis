6b008d565720d7475d62bd67364d1e9090c038a1
Marco FERNANDEZ <fernanm@mh.lvmh>
2022-05-19 19:52:49 -0700

    Adding challenge files

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,61 @@
+---- DELIVERABLE 1
+SELECT emp_no, 
+	first_name,
+	last_name
+FROM employees;
+
+SELECT title, 
+	from_date,
+	to_date
+FROM titles;
+
+-- Retirement titles table
+SELECT e.emp_no, 
+	e.first_name,
+	e.last_name,
+	t.title, 
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees AS e
+	JOIN titles as t
+		ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no ASC;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles AS rt
+WHERE (rt.to_date = '9999-01-01')
+ORDER BY emp_no, rt.to_date DESC;
+
+-- Retiring Titles table
+SELECT COUNT(ut.emp_no),
+    ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY count DESC;
+
+---- DELIVERABLE 2
+-- Mentorship eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+    t.title
+INTO mentorship_eligibility
+FROM employees AS e
+	JOIN dept_emp as de
+		ON (e.emp_no = de.emp_no)
+    JOIN titles as t
+		ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01') 
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
